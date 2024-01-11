1098f88716717c037cce81e5c03f2e4b70bb94c8
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-01-12 10:56:11 +1100

    fix: change return value

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -9,6 +9,6 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 		return (0);
 	}
 
-	result = node->left || node->right ? 1 : 0;
+	result = node->left || node->right ? 0 : 1;
 	return (result);
 }
