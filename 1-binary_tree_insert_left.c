89547a333f9eb77464e5bf23beb0a491be7346b0
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-01-11 21:25:31 +1100

    chore: modify task1 func

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,16 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - set value to the binary_tree_t
- * @parent: parent BT pointer
- * @value: value to be set
+ * binary_tree_insert_left - Inserts a node as the left child of a parent node.
+ * @parent: Pointer to the parent node.
+ * @value: Value to be stored in the new node.
  *
- * Return: pointer of binary_tree_t
+ * Description: This function creates a new node with the given value and
+ * inserts it as the left child of the specified parent node. If the parent
+ * already has a left child, the existing left child becomes the left child of
+ * the new node, and the new node is set as the left child of the parent.
+ *
+ * Return: Pointer to the newly created node, or NULL on failure.
  */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *bt_p;
+	binary_tree_t *bt_p, *parent_left;
 
 	if (parent == NULL)
 		return (NULL);
@@ -23,15 +28,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	bt_p->parent = parent;
 	bt_p->left = NULL;
 	bt_p->right = NULL;
-	if (parent->left)
-	{
-		bt_p->left = parent->left;
-		parent->left = bt_p;
-	}
-	else
-	{
-		parent->left = bt_p;
-	}
+
+	parent_left = parent->left;
+	parent->left = bt_p;
+	bt_p->left = parent_left;
 
 	return (bt_p);
 }
