acbbbef46fb2fbbc30d4394b5086353fd8a64a18
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-01-12 13:35:55 +1100

    docs: add Betty docs

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,5 +1,13 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_inorder - Traverse a binary tree using inorder traversal.
+ * @tree: A pointer to the root node of the tree.
+ * @func: A pointer to a function to call for each node value.
+ *
+ * Return: void
+ */
+
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
