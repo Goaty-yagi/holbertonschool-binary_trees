4860b132d4aa88e195e8d1ba26f941b9453385ab
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-01-12 19:22:52 +1100

    fix: bug

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -19,22 +19,13 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	node_count = 0;
 
 	if (tree->left)
-	{
-		if (tree->left->right || tree->left->left)
-			node_count = node_count + binary_tree_nodes(tree->left);
-		else
-			node_count = node_count + 1;
-	}
+		node_count += binary_tree_nodes(tree->left);
 
 	if (tree->right)
-	{
-		if (tree->right->right || tree->right->left)
-			node_count = node_count + binary_tree_nodes(tree->right);
-		else
-			node_count = node_count + 1;
-	}
-	if (tree->right && tree->left)
-		node_count = node_count + 1;
+		node_count += binary_tree_nodes(tree->right);
+
+	if (tree->left || tree->right)
+		node_count++;
 
 	return (node_count);
 }
