926a165ecf0b45ada6b748c6ba319e6ad784193c
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-01-12 11:00:14 +1100

    docs: add betty docs

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,5 +1,12 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_is_leaf - Check if a node is a leaf in a binary tree.
+ * @node: A pointer to the node to check.
+ *
+ * Return: 1 if the node is a leaf, 0 otherwise.
+ */
+
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	int result;
