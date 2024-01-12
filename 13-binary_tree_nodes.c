f41c12c4b59ce7776960247fa791b2006fbd7d20
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-01-12 18:57:04 +1100

    docs: add betty documentation

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,5 +1,14 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_nodes - Counts the number of nodes in a binary tree with
+ * at least one child.
+ * @tree: A pointer to the root node of the tree.
+ *
+ * Return: The number of nodes in the binary tree with at least one child,
+ *  or 0 if @tree is NULL.
+ */
+
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t node_count;
