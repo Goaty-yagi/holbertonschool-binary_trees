9bd9ee4d6cb875974bdad91562fa7a68fc8d8b67
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-01-12 13:54:48 +1100

    docs: add Betty documentation

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,5 +1,13 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_postorder - Traverse a binary tree using postorder traversal.
+ * @tree: A pointer to the root node of the tree.
+ * @func: A pointer to a function to call for each node value.
+ *
+ * Return: void
+ */
+
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
