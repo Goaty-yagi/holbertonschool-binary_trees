70c1c3120d9302dba8b828f33b6a4e48ebabe2b6
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-01-14 22:51:33 +1100

    fix: divide error condition in to two

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -11,12 +11,15 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
 	binary_tree_t *right_node;
 
-	if (tree == NULL || tree->right == NULL)
+	if (tree == NULL)
+		return (NULL);
+
+	if (tree->right == NULL)
 		return (NULL);
 
 	right_node = tree->right;
 	tree->right = right_node->left;
-	if (tree->right)
+	if (tree->right != NULL)
 	{
 		tree->right->parent = tree;
 	}
