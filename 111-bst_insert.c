9eadac4a2d8a3c31bbe3754624986d794ff0a072
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-01-16 17:11:55 +1100

    chore: fix bud and add Betty docs

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,33 +1,54 @@
 #include "binary_trees.h"
 
+/*
+ * find_bst_insert - Find the correct position to insert
+ * a node in a Binary Search Tree (BST)
+ *
+ * @node: Pointer to the current node in the BST
+ * @new_node: Pointer to the new node to be inserted
+ *
+ * This function recursively searches for the correct position
+ * to insert the new node
+ * in the BST, ensuring that duplicate values are not inserted.
+ * If the value already exists, the new node is freed, and NULL
+ * is returned. Otherwise, the new node is
+ * inserted at the appropriate position and returned.
+ *
+ * Return: Pointer to the inserted node, or NULL if the value already exists or
+ *         memory allocation fails.
+ */
+
 bst_t *find_bst_insert(bst_t *node, bst_t *new_node)
 {
 	if (node == NULL)
 		return (NULL);
 
-	if (node->n > new_node->n && !(find_bst_insert(node->left, new_node)))
+	if (node->n == new_node->n)
 	{
-		if (node->n == new_node->n)
-		{
-			free(new_node);
-			return (NULL);
-		}
-		node->left = new_node;
-		new_node->parent = node;
-		return (new_node);
+		free(new_node);
+		return (NULL);
 	}
-	if (node->n < new_node->n && !(find_bst_insert(node->right, new_node)))
+
+	if (node->n > new_node->n)
 	{
-		if (node->n == new_node->n)
+		if (node->left == NULL)
 		{
-			free(new_node);
-			return (NULL);
+			node->left = new_node;
+			new_node->parent = node;
+			return (new_node);
 		}
-		node->right = new_node;
-		new_node->parent = node;
-		return (new_node);
+		return (find_bst_insert(node->left, new_node));
 	}
-	return (new_node);
+	else
+	{
+		if (node->right == NULL)
+		{
+			node->right = new_node;
+			new_node->parent = node;
+			return (new_node);
+		}
+		return (find_bst_insert(node->right, new_node));
+	}
 }
 
 bst_t *bst_insert(bst_t **tree, int value)
