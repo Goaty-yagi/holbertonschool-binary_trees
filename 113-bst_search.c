818d177608dfb4d39015d7a88b558067607049fd
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-01-17 16:38:18 +1100

    docs: add Betty documentations

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -1,5 +1,13 @@
 #include "binary_trees.h"
 
+/**
+ * bst_search - Searches for a value in a Binary Search Tree (BST)
+ * @tree: Pointer to the root of the BST
+ * @value: Value to search for in the BST
+ *
+ * Return: Pointer to the node containing the value, or NULL if not found
+ */
+
 bst_t *bst_search(const bst_t *tree, int value)
 {
 	if (tree == NULL)
